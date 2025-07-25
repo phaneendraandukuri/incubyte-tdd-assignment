174a1b9fd6d8a9e4901fb35bd230bca91edd08e2
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2025-07-25 17:17:07 +0530

    test: return number for single number string

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -4,4 +4,8 @@ describe('String Calculator', () => {
   test('should return 0 for empty string', () => {
     expect(add('')).toBe(0);
   });
+  test('should return the number itself when only one number is passed', () => {
+    expect(add('4')).toBe(4);
+  });
+
 });
