3657d0d01088da4cf3d4a9f06728280a623594b5
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2025-07-25 17:58:03 +0530

    test: support custom single-character delimiter using // syntax

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -16,5 +16,7 @@ describe('String Calculator', () => {
   test('should support newlines as valid delimiters', () => {
     expect(add('1\n2,3')).toBe(6);
   });
-
+  test('should support custom single-character delimiter using // syntax', () => {
+    expect(add('//;\n1;2')).toBe(3);
+  });
 });
